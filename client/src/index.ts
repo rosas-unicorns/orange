6ba29d652e9a8298f494d36dfaa04e4c1e186119
Lily Li <lily.li0597@gmail.com>
2019-03-15 16:49:01 -0400

    integrated colyseus for multiplayer

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,6 +3,8 @@ import "./index.css";
 import * as BABYLON from "babylonjs";
 import { room } from "./game/network";
 
+var players = {};
+
 // Listen to patches comming
 room.onStateChange.add(function(patches) {
     console.log("Server state changed: ", patches);
@@ -12,36 +14,129 @@ const canvas = document.getElementById('game') as HTMLCanvasElement;
 const engine = new BABYLON.Engine(canvas, true);
 
 // This creates a basic Babylon Scene object (non-mesh)
-var scene = new BABYLON.Scene(engine);
+var createScene = function () {
 
-// This creates and positions a free camera (non-mesh)
-var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    var scene = new BABYLON.Scene(engine);
 
-// This targets the camera to scene origin
-camera.setTarget(BABYLON.Vector3.Zero());
+    // Lights
+    var light0 = new BABYLON.DirectionalLight("Omni", new BABYLON.Vector3(-2, -5, 2), scene);
+    var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(2, -5, -2), scene);
 
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true);
+    // Need a free camera for collisions
+    var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, -8, -20), scene);
+    camera.attachControl(canvas, true);
 
-// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    //Ground
+    var ground = BABYLON.Mesh.CreatePlane("ground", 20.0, scene);
+    ground.material = new BABYLON.StandardMaterial("groundMat", scene);
+    ground.material.backFaceCulling = false;
+    ground.position = new BABYLON.Vector3(5, -10, -15);
+    ground.rotation = new BABYLON.Vector3(Math.PI / 2, 0, 0);
 
-// Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7;
+    room.listen("players/:id", function (change) {
+        if (change.operation === "add") {
+            var box = BABYLON.Mesh.CreateBox("crate", 2, scene);
+            box.material = new BABYLON.StandardMaterial("Mat", scene);
+            box.position = new BABYLON.Vector3(3, 0, -10);
+            box.ellipsoid = new BABYLON.Vector3(1, 1, 1);
 
-// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+            scene.registerBeforeRender(function () {
+                box.moveWithCollisions(scene.gravity);
+            });
 
-// Move the sphere upward 1/2 its height
-sphere.position.y = 1;
+            box.checkCollisions = true;
+            players[change.path.id] = box;
+            
+        } else if (change.operation === "remove") {
+            delete players[change.path.id];
+        }
+    });
 
-// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    room.listen("players/:id/:axis", function (change) {
+        var box = players[change.path.id];
+        if (change.path.axis === "x") {
+            box.moveWithCollisions(new BABYLON.Vector3(-distance, 0, 0))
+        }
+    });
 
-// Attach default camera mouse navigation
-camera.attachControl(canvas);
+
+    //Simple crate
+    // var box = BABYLON.Mesh.CreateBox("crate", 2, scene);
+    // box.material = new BABYLON.StandardMaterial("Mat", scene);
+    // // box.material.diffuseTexture = new BABYLON.Texture("textures/crate.png", scene);
+    // // box.material.diffuseTexture.hasAlpha = true;
+    // box.position = new BABYLON.Vector3(2, -5, -10);
+    // box.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+
+
+    //box2
+    var box1 = BABYLON.Mesh.CreateBox("crate", 2, scene);
+    box1.material = new BABYLON.StandardMaterial("Mat", scene);
+    box1.position = new BABYLON.Vector3(3, 0, -10);
+    box1.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+
+    scene.registerBeforeRender(function () {
+        // box.moveWithCollisions(scene.gravity);
+        box1.moveWithCollisions(scene.gravity);
+    })
+
+    //Set gravity for the scene (G force like, on Y-axis)
+    scene.gravity = new BABYLON.Vector3(0, -0.1, 0);
+
+    // Enable Collisions
+    scene.collisionsEnabled = true;
+
+    //Then apply collisions and gravity to the active camera
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
+
+    //Set the ellipsoid around the camera (e.g. your player's size)
+    camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+
+    //finally, say which mesh will be collisionable
+    ground.checkCollisions = true;
+    // box.checkCollisions = true;
+    box1.checkCollisions = true;
+
+    var inputMap = {};
+    scene.actionManager = new BABYLON.ActionManager(scene);
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function (evt) {
+        inputMap[evt.sourceEvent.key.toLowerCase()] = evt.sourceEvent.type == "keydown";
+    }));
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, function (evt) {
+        inputMap[evt.sourceEvent.key.toLowerCase()] = evt.sourceEvent.type == "keydown";
+    }));
+
+    // Game/Render loop
+    var distance = .1;
+    scene.onBeforeRenderObservable.add(() => {
+        if (inputMap["w"] || inputMap["ArrowUp"]) {
+            // box.moveWithCollisions(new BABYLON.Vector3(0, 0, distance))
+            room.send({ z: distance });
+        }
+        if (inputMap["a"] || inputMap["ArrowLeft"]) {
+            // box.moveWithCollisions(new BABYLON.Vector3(-distance, 0, 0))
+            room.send({ x: distance });
+        }
+        if (inputMap["s"] || inputMap["ArrowDown"]) {
+            // box.moveWithCollisions(new BABYLON.Vector3(0, 0, -distance))
+            room.send({ z: distance });
+        }
+        if (inputMap["d"] || inputMap["ArrowRight"]) {
+            // box.moveWithCollisions(new BABYLON.Vector3(distance, 0, 0))
+            room.send({ x: distance });
+        }
+        if (inputMap[" "]) {
+            // box.moveWithCollisions(new BABYLON.Vector3(0, .4, 0))
+            room.send({ y: distance });
+        }
+    })
+    return scene;
+}
 
 // Scene render loop
+var scene = createScene();
+
 engine.runRenderLoop(function() {
     scene.render();
 });
